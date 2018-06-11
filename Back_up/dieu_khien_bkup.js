29b3a15d2ed1dc25b11a0d783b8431f005cf760e
Du0n9 <34816913+du0n9@users.noreply.github.com>
2018-06-12 01:15:45 +0700

    Initialize

---
+++ Back_up/dieu_khien_bkup.js
@@ -0,0 +1,3 @@
+function dieu_khien(huong) {
+	guiGET("/Web_pages/handle_control?dieu_khien="+huong);
+}
---
+++ thu_muc_va_files.h
@@ -0,0 +1,70 @@
+#ifndef THU_MUC_VA_FILES_H
+#define THU_MUC_VA_FILES_H
+
+#include <FS.h>
+
+#include "config.h"
+
+extern ESP8266WebServer server;
+
+#ifdef DEBUG
+void hien_cac_file() {  
+  Dir dir = SPIFFS.openDir("/");
+  while (dir.next()) {
+    Serial.printf("FS file: %s, size :%d\n", (dir.fileName()).c_str(), dir.fileSize());
+  }
+  Serial.println();
+}
+#endif
+
+String getContentType(String filename){
+  if(server.hasArg("download")) return "application/octet-stream";
+  else if(filename.endsWith(".htm")) return "text/html";
+  else if(filename.endsWith(".html")) return "text/html";
+  else if(filename.endsWith(".css")) return "text/css";
+  else if(filename.endsWith(".js")) return "application/javascript";
+  else if(filename.endsWith(".png")) return "image/png";
+  else if(filename.endsWith(".gif")) return "image/gif";
+  else if(filename.endsWith(".jpg")) return "image/jpeg";
+  else if(filename.endsWith(".ico")) return "image/x-icon";
+  else if(filename.endsWith(".xml")) return "text/xml";
+  else if(filename.endsWith(".pdf")) return "application/x-pdf";
+  else if(filename.endsWith(".zip")) return "application/x-zip";
+  else if(filename.endsWith(".gz")) return "application/x-gzip";
+  return "text/plain";
+}
+
+bool handleFileRead (String path) {
+  #ifdef DEBUG
+  Serial.print("handle file read: " + path + " ... ");
+  #endif
+  if (SPIFFS.exists(path)) {
+    File file = SPIFFS.open(path, "r");
+    #ifdef DEBUG
+    Serial.print("Da mo file; ");
+    #endif
+    
+    server.streamFile (file, getContentType(path));
+    #ifdef DEBUG
+    Serial.print("Da stream file; ");
+    #endif
+    
+    file.close();
+    #ifdef DEBUG
+    Serial.print("Da dong file; ");
+    #endif
+    
+    #ifdef DEBUG
+    Serial.println("done");
+    #endif
+    
+    return true;
+  }
+  #ifdef DEBUG
+  Serial.println("file not found");
+  #endif
+  return false;
+}
+
+
+#endif
